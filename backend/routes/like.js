31e24f0d88fed05dfbe364827381ae0eab7688ef
Goldawn <heunow@gmail.com>
2021-12-08 21:08:31 +0100

    clearing git

--- backend/routes/like.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router({ mergeParams: true});
-
-const likeCtrl = require('../controllers/like');
-
-router.post('/like', likeCtrl.createLike);
-// router.post('/dislike', likeCtrl.dislikePost);
-
-module.exports = router;
